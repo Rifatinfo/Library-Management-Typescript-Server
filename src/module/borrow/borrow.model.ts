fa67ebcb275ccdcfef48193c3eeda036469b73bd
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-06-21 20:54:55 +0600

    borrow model create

---
+++ src/module/borrow/borrow.model.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+import { IBorrow } from "./borrow.interface";
+
+const borrowModel = new Schema<IBorrow>({
+    book : {type : Schema.Types.ObjectId, required : true},
+    quantity : {type : Number, min : 0, required : true},
+    dueDate : {type : Date, required : [true, "On date the book must be returned"]}
+})
+
+const Borrow = model<IBorrow>("Borrow", borrowModel);
+export default Borrow;
