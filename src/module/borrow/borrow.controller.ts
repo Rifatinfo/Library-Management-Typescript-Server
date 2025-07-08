a48ca4db7a57696a8d998c94fbd1657964e8a1ca
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-08 14:27:12 +0600

    add createBorrow update

--- src/module/borrow/borrow.controller.ts
+++ src/module/borrow/borrow.controller.ts
@@ -4,39 +4,37 @@ import Book from "../book/book.model";
 import { Borrow } from "./borrow.model";
 
 const createBorrow = async (req: Request, res: Response) => {
-    try {
-        const borrow = await Borrow.create(req.body);
-        const { book, quantity } = req.body;
-        const foundBook = await Book.findById(book);
-        if (!foundBook) {
-            throw new Error("Book not found");
-        }
-        await foundBook.checkCopies(quantity);
-
-        res.json({
-            success: true,
-            message: "Book borrowed successfully",
-            data: borrow,
-        });
-    } catch (error: any) {
-        if (error.name === "ValidationError") {
-            res.status(400).json({
-                success: false,
-                message: "Validation failed",
-                error: {
-                    name: error.name,
-                    errors: error.errors
-                }
-            });
-        } else {
-            res.status(500).json({
-                success: false,
-                message: "An unexpected error occurred",
-                error: error.message
-            });
-        }
+  try {
+    const borrow = await Borrow.create(req.body);
+    const { book, quantity } = req.body;
+    const foundBook = await Book.findById(book);
+    if (!foundBook) {
+      throw  ("Book not found") 
     }
+    if (foundBook.copies < quantity) {
+       throw  ( "Not enough copies available") 
+    }
+    foundBook.copies -= quantity;
+    if (foundBook.copies <= 0) {
+      foundBook.available = false;
+    }
+    await foundBook.save();
+    res.json({
+      success: true,
+      message: "Book borrowed successfully",
+      data: borrow,
+    });
+
+  } catch (error: any) {
+    console.error("Error in createBorrow:", error);
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred",
+      error: error.message,
+    });
+  }
 };
+
 const getBorrow = async (req: Request, res: Response) => {
     try {
         const borrow = await Borrow.aggregate([
